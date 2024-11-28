18765c98c9a330ae1150e80ab5d576fd71696587
David O Connor <123404234+davidoc23@users.noreply.github.com>
2024-11-28 10:43:04 +0000

    feat: add selected item styling and include settings icon in tabs

--- ui-components-demo/src/app/tabs/tabs.page.ts
+++ ui-components-demo/src/app/tabs/tabs.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonBadge } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { home, person, notifications } from 'ionicons/icons';
+import { home, person, notifications, settings } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
@@ -13,6 +13,6 @@ export class TabsPage {
   notificationCount = 3; // This will be displayed in the badge
   
   constructor() {
-    addIcons({ home, person, notifications });
+    addIcons({ home, person, notifications, settings });
   }
 }
